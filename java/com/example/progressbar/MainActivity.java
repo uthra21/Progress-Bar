6e4a062478c2ca74fe05b1912d9377eb01d9d27d
uthra21 <uthra21247@gmail.com>
2024-07-20 12:25:38 +0530

    Add files via upload

---
+++ java/com/example/progressbar/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.progressbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+
+public class MainActivity extends AppCompatActivity {
+
+    ProgressBar progressBar;
+    Button button;
+
+    int progress = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        progressBar=findViewById(R.id.progressBar);
+        button=findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                progress=progress+10;
+
+                progressBar.setProgress(progress);
+
+            }
+        });
+    }
+}
